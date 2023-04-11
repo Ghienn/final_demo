7bf756c6c4270dd60e6466293c1276e84e0d3355
Ghienn <hienhine0310@gmail.com>
2023-04-11 16:48:00 +0700

    update

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const camera = new BABYLON.ArcRotateCamera(
   -Math.PI / 2.5, // alpha
   Math.PI / 3,  // beta
   4,            // radius
-  new BABYLON.Vector3(-100, 180, 0),
+  new BABYLON.Vector3(-200, 50, 0),
   scene
 );
-camera.setPosition(new BABYLON.Vector3(300, 500, 600));
+camera.setPosition(new BABYLON.Vector3(100, 300, 600));
 
 camera.attachControl(canvas, true);
 
@@ -25,9 +25,6 @@ const light = new BABYLON.HemisphericLight(
   scene
 );
 scene.clearColor = new BABYLON.Color3(0.95, 0.95, 0.95);
-
-// // Create ground
-// const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
 
 // Create bookshelf
 let bookshelf;
@@ -41,81 +38,100 @@ function createBookshelf(height, width, depth, numShelves, numColumns) {
   // Create bookshelf mesh
   bookshelf = new BABYLON.Mesh("bookshelf", scene);
 
+
   //Texture
-  const material = new BABYLON.StandardMaterial("wood", scene);
+  // const material = new BABYLON.StandardMaterial("wood", scene);
   const shelfMaterial = new BABYLON.StandardMaterial("backSideMate");
-  shelfMaterial.diffuseTexture = new BABYLON.Texture("Wood-Veneer-Architextures.jpg", scene);
+  currentTexture =  shelfMaterial.diffuseTexture = new BABYLON.Texture("Wood-Veneer-Architextures.jpg", scene);
+
+  // currentTexture = box.material.diffuseTexture = new BABYLON.Texture("texture1.jpg", scene);
+  
 
   // Create shelves
   const shelfHeight = 5;
-  for (let i = 0; i < numShelves +1 ; i++) {
-    const shelf = BABYLON.MeshBuilder.CreateBox(
-      `shelf-${i}`,
-      { height: shelfHeight, width, depth },
-      scene
-    );
-
-    shelf.position.y = i * height;
-    shelf.parent = bookshelf;
-    shelf.material = shelfMaterial;
-  }
-
-
-  // Create Columns
-
-  // const columnHeight = (numShelves - 1)  * height;
-  // const columnWidth = (numColumns - 1)  * height;
-  // for (let i = 0; i < numColumns - 1; i++) {
-  //   const column = BABYLON.MeshBuilder.CreateBox(
-  //     `column-${i}`,
-  //     { height: columnHeight, width: 5, depth },
-  //     scene
-  //   );
-
-  //   column.position.x = i * -columnWidth;
-  //   column.position.y = columnHeight / 2 ;
-  //   column.parent = bookshelf;
-  // }
-
-
-  // Create sides
-
   const sideThickness = 5;
   const sideWidth = numShelves  * height;
-  const leftSide = BABYLON.MeshBuilder.CreateBox(
-    "left-side",
-    { height: sideWidth + sideThickness, width: sideThickness, depth },
+
+  // Create Columns, Shelves, backside
+    for (let i = 0; i < numColumns+1; i++) {
+
+      for (let j = 0; j < numShelves +1 ; j++) {
+        const shelf = BABYLON.MeshBuilder.CreateBox(
+          `shelf-${j}`,
+          { height: shelfHeight, width: i*width + sideThickness, depth },
+          scene
+        );
+        shelf.position.x = -i*width/2;
+        shelf.position.y = j * height;
+        shelf.parent = bookshelf;
+        shelf.material = shelfMaterial;
+      }
+
+      const column = BABYLON.MeshBuilder.CreateBox(
+        "left-side",
+        { height: sideWidth + sideThickness, width: sideThickness, depth },
+        scene
+      );
+      column.position.x = -width*i;
+      column.position.y = sideWidth / 2;
+      column.parent = bookshelf;
+      column.material = shelfMaterial;
+
+  const backSide = BABYLON.MeshBuilder.CreateBox(
+    "back-side",
+    { height: sideWidth + sideThickness, width: i*width + sideThickness, depth: sideThickness },
     scene
   );
-  leftSide.position.x = -(width / 2);
-  leftSide.position.y = sideWidth / 2;
-  leftSide.parent = bookshelf;
-  leftSide.material = shelfMaterial;
 
+  backSide.position.x = -width/2 * i;
+  backSide.position.y = sideWidth / 2;
+  backSide.position.z = -depth/2;
+  backSide.parent = bookshelf;
+  backSide.material = shelfMaterial;
+  }
+
+  // Create sides
   const rightSide = BABYLON.MeshBuilder.CreateBox(
     "right-side",
     { height: sideWidth + sideThickness, width: sideThickness, depth },
     scene
   );
-  rightSide.position.x = width / 2;
+  rightSide.position.x = -width;
   rightSide.position.y = sideWidth / 2;
   rightSide.parent = bookshelf;
   rightSide.material = shelfMaterial;
 
-  const backSide = BABYLON.MeshBuilder.CreateBox(
-    "back-side",
-    { height: sideWidth + sideThickness, width: width + sideThickness, depth: sideThickness },
+  const leftSide = BABYLON.MeshBuilder.CreateBox(
+    "left-side",
+    { height: sideWidth + sideThickness, width: sideThickness, depth },
     scene
   );
+  leftSide.position.x = 0;
+  leftSide.position.y = sideWidth / 2;
+  leftSide.parent = bookshelf;
+  leftSide.material = shelfMaterial;
 
-  backSide.position.x = 0;
-  backSide.position.y = sideWidth / 2;
-  backSide.position.z = -depth/2;
-  backSide.parent = bookshelf;
-  backSide.material = shelfMaterial;
+  var textureElements = document.querySelectorAll(".textures img");
+for (var i = 0; i < textureElements.length; i++) {
+    var element = textureElements[i];
+    element.addEventListener("click", function(event) {
+        var newTexturePath = event.target.src;
+        var newTexture = new BABYLON.Texture(newTexturePath, scene);
+        currentTexture = shelfMaterial.diffuseTexture = newTexture;
+        // Deselect all other textures
+        for (var j = 0; j < textureElements.length; j++) {
+            textureElements[j].classList.remove("selected");
+        }
+        // Select the clicked texture
+        event.target.classList.add("selected");
+    }, false);
+};
 }
 
-createBookshelf(100, 300, 100, 4, 2);
+createBookshelf(100, 100, 100, 2, 2);
+  
+
+
 
 // Update bookshelf on button click
 const updateButton = document.getElementById("update");
@@ -125,7 +141,6 @@ updateButton.addEventListener("click", () => {
   const depth = Number(document.getElementById("shelf-depth").value);
   const numShelves = Number(document.getElementById("num-shelves").value);
   const numColumns = Number(document.getElementById("num-columns").value);
-
   createBookshelf(height, width, depth, numShelves, numColumns);
 });
 
